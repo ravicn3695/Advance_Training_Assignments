a0d9eb39dcd5193cce477393f6d0170a574b5e73
ravicn3695 <raviravicn3@gmail.com>
2022-04-29 13:37:49 +0530

    Repeating Elements and Nth Node

---
+++ Day_2_projects/src/com/statement12/ravi/RepeatingElement/FirstRepeatingElement.java
@@ -0,0 +1,46 @@
+package com.statement12.ravi.RepeatingElement;
+
+import java.util.HashSet;
+
+public class FirstRepeatingElement {
+
+		
+	    static void printFirstRepeating(int arr[])
+	    {
+	       
+	        int min = -1;
+	 
+
+	        HashSet<Integer> set = new HashSet<>();
+	 
+	       
+	        for (int i=arr.length-1; i>=0; i--)
+	        {
+	            
+	            if (set.contains(arr[i]))
+	                min = i;
+	 
+	            else   
+	                set.add(arr[i]);
+	        }
+	 
+	       
+	        if (min != -1)
+	          System.out.println("The first repeating element is " + arr[min]);
+	        else
+	          System.out.println("There are no repeating elements");
+	    }
+	 
+	    
+public static void main (String[] args) throws java.lang.Exception
+	    {
+	        int arr[] = {1, 2, 3, 10, 2, 1, 4, 5, 7, 8 };
+	        printFirstRepeating(arr);
+	        
+	        int arr1[] = {1, 2, 3, 10, 6, 4, 3, 7, 10};
+	        printFirstRepeating(arr1);
+	        
+	        
+	    }
+
+}
